4c1f5e3f5b498487c1db0fbaafec738e0aa7481b
GurungRegina <gina524286@gmail.com>
2019-01-22 14:46:12 +0000

    adding playbooks for ansible and schemas for bq tables

---
+++ schema/schema.sh
@@ -0,0 +1,4 @@
+
+bq mk --schema etd_eta_changed.json -t invertible-star-147708:eventstream.etd_eta_changed
+bq mk --schema purchase_order_approved.json -t invertible-star-147708:eventstream.purchase_order_approved
+bq mk --schema quantity_moved_in_to_new_po.json -t invertible-star-147708:eventstream.quantity_moved_in_to_new_po
